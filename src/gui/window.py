c518dc9033588b49c90615afa0a0ee223a2299cf
Reim-developer <contact.kaxtr@gmail.com>
2025-07-04 00:12:44 +0700

    Refactor Window class to use alias module and improve screen centering logic

--- src/gui/window.py
+++ src/gui/window.py
@@ -1,13 +1,37 @@
-from type import(
-    MainWindow, APP_NAME, APP_WIDTH, APP_HEIGHT
+from alias import (
+    Application, Critical, MainWindow
+)
+from constains import (
+    APP_NAME, APP_VERSION, APP_WIDTH, APP_HEIGHT
 )
 
 class Window(MainWindow):
     def __init__(self) -> None:
         super().__init__()
 
-    def InitApplication(self) -> None:
-        self.setWindowTitle(APP_NAME)
-        self.setMinimumSize(APP_WIDTH, APP_HEIGHT)
+    def __centerScreen(self) -> None:
+        application = Application.primaryScreen()
 
+        if not application:
+            Critical("[Class Window] Application Primary Screen is None.")
+            return
+        
+        screen = application.availableGeometry()
+        window = self.geometry()
+
+        windowWidth = window.width()
+        windowHeight = window.height()
+
+        screenWidth = screen.width()
+        screenHeight = screen.height()
+
+        xLocation = (screenWidth - windowWidth) // 2
+        yLocation = (screenHeight - windowHeight) // 2
+
+        self.move(xLocation, yLocation)
+
+    def InitApplication(self) -> None:
+        self.setWindowTitle(f"{APP_NAME} | {APP_VERSION}")
+        self.setMinimumSize(APP_WIDTH, APP_HEIGHT)
+        self.__centerScreen()
         self.show()
