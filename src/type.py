e1ba768fee2150edbe0240f5db056d825b4ae065
Reim-developer <contact.kaxtr@gmail.com>
2025-07-03 23:53:37 +0700

    Add type.py to define application constants and PyQt6 components

---
+++ src/type.py
@@ -0,0 +1,15 @@
+import sys
+from PyQt6.QtWidgets import (
+    QApplication, QMainWindow
+)
+
+
+Appliation = QApplication
+MainWindow = QMainWindow
+
+SysArgv = sys.argv
+SysExit = sys.exit
+
+APP_NAME: str = "Voice Bot"
+APP_WIDTH: int = 600
+APP_HEIGHT: int = 600
